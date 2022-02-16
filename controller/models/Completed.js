7a1bcc91a686c8c6e428a7f637bf8f0416434628
Denis <d.igoschin.igoshin@gmail.com>
2022-02-16 20:04:07 +0300

    created model changed

---
+++ controller/models/Completed.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const CompletedScheme = mongoose.Schema({
+  master: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  test: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Test",
+    required: true,
+  },
+  Score: {
+    type: Number,
+    required: true,
+  },
+  Date: {
+    type: Date,
+    required: true,
+  },
+});
+
+const Completed = (module.exports = mongoose.model("Course", CompletedScheme));
