f6e4a2539575dbd106a4ea5f7d476f6a05ef391a
Denis <d.igoschin.igoshin@gmail.com>
2022-02-16 19:37:44 +0300

    model test has been changed

--- controller/models/Test.js
+++ controller/models/Test.js
@@ -4,14 +4,21 @@ const TestScheme = mongoose.Schema({
   title: {
     type: String,
     required: true,
-    unique: true,
   },
   category: {
     type: String,
     required: true,
   },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
   language: {
     type: String,
+    required: true,
+  },
+  isGeneral: {
+    type: Boolean,
     required: true,
   },
   words: {
@@ -22,19 +29,19 @@ const TestScheme = mongoose.Schema({
 
 const Test = (module.exports = mongoose.model("Test", TestScheme));
 
-module.exports.getTestByTitle = async (title, callback) => {
-  const query = { title };
-  await Test.findOne(query, callback);
-};
+// module.exports.getTestByTitle = async (title, callback) => {
+//   const query = { title };
+//   await Test.findOne(query, callback);
+// };
 
 module.exports.addTest = async (newTest, callback) => {
   await newTest.save(callback);
 };
 
-module.exports.TITLE_SALT = "usertests";
-module.exports.TITLE_SEPARATOR = "_";
+// module.exports.TITLE_SALT = "usertests";
+// module.exports.TITLE_SEPARATOR = "_";
 
-module.exports.getTestsInBook = async user => {
-  const tests = await Test.find({title: {$in: [...user.book]}})
-  return tests
-};
+// module.exports.getTestsInBook = async user => {
+//   const tests = await Test.find({title: {$in: [...user.book]}})
+//   return tests
+// };
