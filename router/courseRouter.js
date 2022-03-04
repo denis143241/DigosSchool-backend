37cd68bba033346e343edc97095320fed2e46cc7
Denis <d.igoschin.igoshin@gmail.com>
2022-03-04 19:17:51 +0300

    liderboard has done

--- router/courseRouter.js
+++ router/courseRouter.js
@@ -31,4 +31,30 @@ router.post("/add-test", authMiddleware, (req, res) => {
     })
 })
 
+/**
+ * Возвращает JSON таблицу лидеров определенного курса
+ * 
+ * @param {import("mongoose").ObjectId} courseId 
+ */
+router.get("/liderboard", authMiddleware, async (req, res) => {
+    /*
+    Зная ID участника обойти все завершенные тесты и взять оттуда ТЕ, которые с нужным UserID и включает в себя тест из курса.
+    Забираем Score и повторяем действие пока не найдем все завершенные по Одному Юзеру
+    Повторяем действие пока не пройдем всех участников курса.
+
+    Итог Таблца -> Юзернейм | Балл -> По убыванию 
+    [
+        {
+            user: Some,
+            totalScore: Any
+        }
+    ]
+    */
+    Course.liderboard(req.body.courseId, (answer) => {
+        // Сортировка по возрастанию
+        answer = answer.sort((a, b) => b.totalScore - a.totalScore);
+        res.json({result: answer})
+    })
+})
+
 module.exports = router
