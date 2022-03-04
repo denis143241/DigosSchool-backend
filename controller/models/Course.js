f591c33bf118d7c1c40e27f2cef484da5da2b5c7
Denis <d.igoschin.igoshin@gmail.com>
2022-03-04 19:17:51 +0300

    liderboard has been done

--- controller/models/Course.js
+++ controller/models/Course.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const User = require("./User")
+const Completed = require("./Completed")
 
 const CourseScheme = mongoose.Schema({
   creator: {
@@ -36,4 +38,44 @@ module.exports.addTest = (courseId, testId, callback) => {
 
     Course.updateOne({_id: courseId}, {$set: {tests: [...course.tests, testId]}}, callback)
   })
+}
+
+/**
+ * Возвращает таблицу лидеров определенного курса
+ * 
+ * @param {import("mongoose").ObjectId} courseId
+ * @param {function} callback
+ */
+module.exports.liderboard = async (courseId, callback) => {
+  const output = []
+
+  await Course.findById(courseId, async (err, c) => {
+    if (err) throw err
+
+    const users = await User.getUserByCourse_many(courseId)
+
+    for (let i = 0; i < users.length; i++) {
+      let u = users[i]
+      let userCompletes = await Completed.find({$and: [{master: u._id}, {test: {$in: c.tests}}]}, {_id: 0, Score: 1, test: 1})
+      userCompletes = remainSuccessfulyAttemp(userCompletes)
+      let totalScore = 0
+      if (userCompletes.length > 0) {
+        totalScore = userCompletes.map(el => el[1]).reduce((total, current) => total + current)
+      }
+      output.push({user: u.username, totalScore})
+    }
+    callback(output)
+  })
+}
+
+/**
+ * @param {Array} arr [{test: ObjectId, Score: Number}, ...]
+ * @returns {Array} Object.entries
+ */
+function remainSuccessfulyAttemp(arr) {
+  const dict = {}
+  arr.forEach(el => {
+    dict[el.test] = dict[el.test] ? Math.max(dict[el.test], el.Score) : el.Score
+  })
+  return Object.entries(dict)
 }
